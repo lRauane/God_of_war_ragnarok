0c4834005358c95d378cbdef592ef619cebd247f
Rauane Lima <rauanelima7@gmail.com>
2022-11-23 13:10:34 -0300

    modal para trailer

--- components/js/main.js
+++ components/js/main.js
@@ -3,3 +3,16 @@ var swiper = new Swiper(".slider-characters", {
   spaceBetween: 19,
   freeMode: true,
 });
+
+// modal player
+const modalBtn = document.querySelector('.modal-btn')
+const modal = document.querySelector('.modal-overlay')
+const closeBtn = document.querySelector('.close-btn')
+
+modalBtn.addEventListener('click', () => {
+  modal.classList.add('open-modal');
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('open-modal');
+});
